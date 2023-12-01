954d93886340d8b2a8c1ab8b511ce8df34f4b6ed
pyragnis <enzosabbatorsi@outlook.fr>
2023-12-01 16:15:17 +0100

    add list

---
+++ src/pages/ListAlbum/index.js
@@ -0,0 +1,17 @@
+// pages/Login/index.js
+import React from 'react';
+import Navbar from '../../components/VerticalNavbar';
+import ThreeLogo from '../../components/ThreeLogo';
+
+
+
+const ListeAlbum = () => {
+  return (
+    <div className="Accueil">
+      <Navbar />
+      
+    </div>
+  );
+};
+
+export default ListeAlbum;
---
+++ src/pages/ListSong/index.js
@@ -0,0 +1,17 @@
+// pages/Login/index.js
+import React from 'react';
+import Navbar from '../../components/VerticalNavbar';
+import ThreeLogo from '../../components/ThreeLogo';
+
+
+
+const ListSong = () => {
+  return (
+    <div className="Accueil">
+      <Navbar />
+      
+    </div>
+  );
+};
+
+export default ListSong;
